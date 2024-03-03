e07055a95a70a8d085bd4e0269ac4ba0414440cd
Oviedo Sergio <oviedosdp@gmail.com>
2024-03-03 01:19:34 -0500

    taskslist mejorados para celulares

--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -11,7 +11,7 @@ function TaskCard({ task }) {
   };
 
   return (
-      <div className="bg-gray-800 text-white p-4 rounded-md">
+      <div className="bg-gray-800 text-white p-4 rounded-md min-w-[10rem]">
         <h2 className="text-xl font-bold capitalize">{task.title}</h2>
         <p className="text-gray-500 text-sm">{task.description}</p>
         <button className="bg-red-500 px-2 py-1 rounded-md mt-4 hover:bg-red-400" onClick={handleDelete}>Eliminar Tarea</button>
--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -9,7 +9,7 @@ function TaskList() {
   if (!tasks || tasks.length === 0) return <h1 className="text-white text-4xl font-bold text-center">No hay Tareas aun</h1>;
 
   return (
-    <div className="grid grid-cols-4 gap-2">
+    <div className="grid grid-cols-auto gap-2">
       {tasks.map((task) => (
         <TaskCard key={task.id} task={task} />
       ))}
